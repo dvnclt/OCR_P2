8ffd31030590520e4746119af32b8e2186844107
Damien Vincelot <damien.vincelot@gmail.com>
2024-03-19 15:46:36 +0100

    Ajout de la fonctionnalité chargement Load CSV

--- etape_1.py
+++ etape_1.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import csv
 
 # Lien de la page à scraper
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
@@ -14,53 +15,71 @@ page = reponse.content
 soup = BeautifulSoup(page, "html.parser")
 
 # Recherche Title
+product_title_list = []
 product_title = soup.find("h1").string
-
-print(f"Title : '{product_title}'")
+product_title_list.append(product_title)
 
 # Recherche Product page url
+product_page_url_list = []
 product_page_url = url
-print(f"Page product URL : '{product_page_url}'")
+product_page_url_list.append(product_page_url)
 
 # Recherche UPC
+universal_product_code_list = []
 universal_product_code = soup.find("td").string
-print(f"Universal product code : '{universal_product_code}'")
+universal_product_code_list.append(universal_product_code)
 
 # Recherche Price Including Tax
+price_including_tax_list = []
 price_including_tax = soup.find("p", class_="price_color").string
-print(f"Price including tax : '{price_including_tax}'")
+price_including_tax_list.append(price_including_tax)
 
 # Recherche Price Excluding Tax
+price_excluding_tax_list = []
 price_excluding_tax = soup.find('table', class_='table').find_all('td')[2].string
-print(f"Price excluding tax : '{price_excluding_tax}'")
+price_excluding_tax_list.append(price_excluding_tax)
 
 # Recherche Number available
+number_available_list = []
 number_available = soup.find("p", class_="instock availability").text.strip()
-print(f"Number available : '{number_available}'")
+number_available_list.append(number_available)
 
 # Recherche Product description
+product_description_list = []
 product_description = soup.find("meta", attrs={"name": "description"}).get("content","").strip()
-print(f"Product Description : '{product_description}'")
+product_description_list.append(product_description)
 
 # Recherche Category
+category_list = []
 category = soup.find("ul", class_="breadcrumb").find_all("li")[2].text.strip()
-print(f"Category : '{category}'")
+category_list.append(category)
 
 # Recherche Review Rating
+review_rating_list = []
 star_class_name = "star-rating Three"
-review_rating = soup.find("p", class_=star_class_name).text
+review_rating = soup.find("p", class_=star_class_name)
 if "One" in star_class_name:
-    print("Review Rating : 1/5")
+    review_rating_list.append("1/5")
 elif "Two" in star_class_name:
-    print("Review Rating : 2/5")
+    review_rating_list.append("2/5")
 elif "Three" in star_class_name:
-    print("Review Rating : 3/5")
+    review_rating_list.append("3/5")
 elif "Four" in star_class_name:
-    print("Review Rating : 4/5")
+    review_rating_list.append("4/5")
 else :
-    print("Review Rating : 5/5")
+    review_rating_list.append("5/5")
+
 
 # Recherche Lien image
+image_url_list = []
 base_url = "https://books.toscrape.com/"
 image_url = urljoin(base_url, soup.find("div", class_="item active").find("img")["src"])
-print(f"Lien de l'image : '{image_url}'")
+image_url_list.append(image_url.strip())
+
+# Création et écriture des données extraites dans un fichier CSV
+en_tete = ["Title", "Product page URL", "UPC", "Price incl. Tax", "Price excl. Tax", "Number available", "Product Description", "Category", "Review Rating"]
+with open("etape_1.csv", "w") as etape_1_csv_file:
+    writer = csv.writer(etape_1_csv_file, delimiter=",")
+    writer.writerow(en_tete)
+    for element in zip (product_title_list, product_page_url_list, universal_product_code_list, price_including_tax_list, price_excluding_tax_list, number_available_list, product_description_list, category_list, review_rating_list):
+        writer.writerow([product_title, product_page_url, universal_product_code, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating_list])
