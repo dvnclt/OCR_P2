1d32f9b1e5fdf6ee785e56ba276eeb7bcee21941
Damien Vincelot <damien.vincelot@gmail.com>
2024-03-26 17:40:53 +0100

    maj du script : etapes 3 et 4 du projet complêtées

--- etape_3.py
+++ etape_3.py
@@ -1,29 +1,58 @@
 # Import des modules nécessaires
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-import csv
+from pathlib import Path
+import time
+from urllib.parse import urljoin # A supprimer
+#import csv / A voir si suppression possible
 from utils import collect_product_urls, collect_product_data, write_data_csv
-from utils import product_link_list
+from utils import product_link_list, product_title_list, product_page_url_list, universal_product_code_list, price_including_tax_list, price_excluding_tax_list, number_available_list, product_description_list, category_list, review_rating_list, image_url_list, count
+
+start_time = time.time()
 
 category_link_list = []
 
-# Obtention du contenu de la page
-reponse = requests.get("https://books.toscrape.com/catalogue/page-1.html")
-
-# Analyse de la page html avec BeautifulSoup
-soup = BeautifulSoup(reponse.content, "html.parser")
-
-# Recherche de l'url de chaque catégorie
-category_link = soup.find("ul", class_="nav nav-list").find("ul").find_all("a", href=True)
-
-for category_item in category_link:
-    full_category_link = "https://books.toscrape.com/catalogue/" + category_item['href']
-    category_link_list.append(full_category_link)
-    collect_product_urls(full_category_link)
+total_products_added = 0
+output_folder = Path("csv_files_directory")
+output_folder.mkdir(parents=True, exist_ok=True)
 
 
-for product_item in product_link_list:
-    collect_product_data(product_item)
+response = requests.get("https://books.toscrape.com/catalogue/page-1.html") # Obtention du contenu de la page
 
-write_data_csv()
+soup = BeautifulSoup(response.content, "html.parser") # Parse le contenu de la page html
+category_link = soup.find("ul", class_="nav nav-list").find("ul").find_all("a", href=True) # Recherche de l'url de chaque catégorie
+
+for category_item in category_link: # Boucle qui pour chaque lien de catégorie trouvé dans la page d'accueil :
+    full_category_link = "https://books.toscrape.com/catalogue/" + category_item['href'] # Recompose le lien de catégorie trouvé pour qu'il soit exploitable"
+    category_link_list.append(full_category_link) # Ajoute le lien exploitable à la liste category_link_list
+    category_csv_name = full_category_link.split('/')[-2].split('_')[0] # Sélectionne, dans le lien exploitable, l'intitulé pour une catégorie donnée
+    collect_product_urls(full_category_link) # Appel de la fonction de collecte de l'ensemble des liens de produits pour une catégorie donnée
+
+    for product_item in product_link_list: # Pour chaque lien produit trouvé dans la fonction précédente :
+        collect_product_data(product_item) # Appelle la fonction de collecte de l'ensemble des données pour un produit donné
+    
+    csv_filename = output_folder / f"{category_csv_name}.csv" # Variable contenant le chemin et le nom du fichier à créer.
+    write_data_csv(csv_filename) # Appel de la fonction de création et d'écriture de fichier .csv avec les données collectées
+
+    # Lorsqu'un fichier csv est créé, réinitialisation des listes pour que chaque fichier csv ne comporte que les données de sa catégorie
+    product_link_list.clear()
+    product_title_list.clear()
+    product_page_url_list.clear()
+    universal_product_code_list.clear()
+    price_including_tax_list.clear()
+    price_excluding_tax_list.clear()
+    number_available_list.clear()
+    product_description_list.clear()
+    category_list.clear()
+    review_rating_list.clear()
+    image_url_list.clear()
+
+if len(count) != 1000:
+    print("Erreur : tous les produits n'ont pas été trouvés")
+else :
+    print("Tous les produits ont bien été ajoutés")
+
+end_time = time.time()
+execution_time = (end_time - start_time) / 60
+
+print("Temps d'exécution du programme : ", execution_time, "minutes")
--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
 # Import des modules nécessaires
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin # méthode à remplacer
 import csv
 
-category_link_list = []
 product_link_list = []
 product_title_list = []
 product_page_url_list = []
@@ -16,60 +15,65 @@ product_description_list = []
 category_list = []
 review_rating_list = []
 image_url_list = []
-product_data_list = product_title_list, product_page_url_list, universal_product_code_list, price_including_tax_list, price_excluding_tax_list, number_available_list, product_description_list, category_list,review_rating_list, image_url_list
+count = []
+
 
 def collect_product_urls(full_category_link):
 
-    reponse = requests.get(full_category_link)
+    category_page = [full_category_link]
+    category_name = full_category_link.split('/')[-2].split('_')[0].capitalize()
+    total_product_link_by_category = 0
+  
+    while category_page:
+        current_page = category_page.pop(0)
+        response = requests.get(current_page)
+        soup = BeautifulSoup(response.content, "html.parser")
+        product_link = soup.find_all("a", title = True)
 
-    soup = BeautifulSoup(reponse.content, "html.parser")
-    product_link = soup.find_all("a", title = True)
+        for product_item in product_link:
+            full_product_link = product_item["href"].replace("../../../", "https://books.toscrape.com/catalogue/")
+            product_link_list.append(full_product_link)
+            count.append(full_product_link)
+            total_product_link_by_category += 1
 
-    print("Nombre de liens de produit trouvés dans la catégorie donnée:", len(product_link))
+        next_page = soup.find("li", class_="next")
+        if next_page:
+            next_page_link = next_page.find("a")["href"]
+            next_page_url = urljoin(current_page, next_page_link)
+            category_page.append(next_page_url)
 
-    for product_item in product_link:
-        full_product_link = product_item["href"].replace("../../../", "https://books.toscrape.com/catalogue/")
-        product_link_list.append(full_product_link)
-
-    print("Nombre total de liens de produit ajoutés à la liste 'product_link_list'", len(product_link_list))
+    print(f"Nombre de produits trouvés dans la catégorie {category_name}:", total_product_link_by_category)
+    print("Nombre total de produits trouvés : ", len(count))
 
 def collect_product_data(product_item):
     
-    reponse = requests.get(product_item)
+    response = requests.get(product_item)
 
-    soup = BeautifulSoup(reponse.content, "html.parser")
+    soup = BeautifulSoup(response.content, "html.parser")
 
     product_title = soup.find("h1").string
     product_title_list.append(product_title)
-    print("Nombre total de titres ajoutés à la liste", len(product_title_list))
 
     product_page_url = product_item
     product_page_url_list.append(product_page_url)
-    print("Nombre total de liens produits ajoutés à la liste", len(product_page_url_list))
 
     universal_product_code = soup.find("td").string
     universal_product_code_list.append(universal_product_code)
-    print("Nombre total d'UPC ajoutés à la liste", len(universal_product_code_list))
 
     price_including_tax = soup.find("p", class_="price_color").string
     price_including_tax_list.append(price_including_tax)
-    print("Nombre total de price including tax ajoutés à la liste", len(price_including_tax_list))
 
     price_excluding_tax = soup.find('table', class_='table').find_all('td')[2].string
     price_excluding_tax_list.append(price_excluding_tax)
-    print("Nombre total de price excluding tax à la liste", len(price_excluding_tax_list))
 
     number_available = soup.find("p", class_="instock availability").text.strip()
     number_available_list.append(number_available)
-    print("Nombre total de number available ajoutés à la liste", len(number_available_list))
 
     product_description = soup.find("meta", attrs={"name": "description"}).get("content","").strip()
     product_description_list.append(product_description)
-    print("Nombre total de descriptions ajoutées à la liste", len(product_description_list))
 
     category = soup.find("ul", class_="breadcrumb").find_all("li")[2].text.strip()
     category_list.append(category)
-    print("Nombre total de catégories ajoutées à la liste", len(category_list))
 
     rating = str(soup.find("div", class_="col-sm-6 product_main").find_all("p")[2])
     if "One" in rating:
@@ -82,17 +86,15 @@ def collect_product_data(product_item):
         review_rating_list.append("4/5")
     else :
         review_rating_list.append("5/5")
-    print("Nombre total de reviews ajoutées à la liste", len(review_rating_list))
 
     image_base_url = "https://books.toscrape.com/"
     image_url = urljoin(image_base_url, soup.find("div", class_="item active").find("img")["src"])
     image_url_list.append(image_url.strip())
-    print("Nombre total de liens d'image ajoutés à la liste", len(image_url_list))
 
-def write_data_csv():
+def write_data_csv(csv_filename):
     en_tete = ["Title", "Product page URL", "UPC", "Price incl. Tax", "Price excl. Tax", "Number available", "Product Description", "Category", "Review Rating", "Product image url"]
-    with open("etape_3.csv", "w") as etape_3_csv_file:
-        writer = csv.writer(etape_3_csv_file, delimiter=",")
+    with open(csv_filename, "w") as csv_file:
+        writer = csv.writer(csv_file, delimiter=",")
         writer.writerow(en_tete)
         for i in range(len(product_title_list)):
             row = [product_title_list[i], product_page_url_list[i], universal_product_code_list[i], price_including_tax_list[i], price_excluding_tax_list[i], number_available_list[i], product_description_list[i], category_list[i], "'" + review_rating_list[i] + "'", image_url_list[i]]
