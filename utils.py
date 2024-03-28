9117accbc83e4fe41c0ec194c6685d4e40f067c5
Damien Vincelot <damien.vincelot@gmail.com>
2024-03-28 22:48:25 +0100

    booktoscrape.py dependancie

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 # Import des modules nécessaires
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin # méthode à remplacer
 import csv
+import os
+import re
 
 product_link_list = []
 product_title_list = []
@@ -39,7 +40,8 @@ def collect_product_urls(full_category_link):
         next_page = soup.find("li", class_="next")
         if next_page:
             next_page_link = next_page.find("a")["href"]
-            next_page_url = urljoin(current_page, next_page_link)
+            url_part = current_page.split("/")[-1]
+            next_page_url = current_page.replace(url_part, next_page_link)
             category_page.append(next_page_url)
 
     print(f"Nombre de produits trouvés dans la catégorie {category_name}:", total_product_link_by_category)
@@ -87,15 +89,31 @@ def collect_product_data(product_item):
     else :
         review_rating_list.append("5/5")
 
-    image_base_url = "https://books.toscrape.com/"
-    image_url = urljoin(image_base_url, soup.find("div", class_="item active").find("img")["src"])
+    image_url = "https://books.toscrape.com/" + soup.find("div", class_="item active").find("img")["src"]
     image_url_list.append(image_url.strip())
 
-def write_data_csv(csv_filename):
+def write_data_csv(csv_folder, category_csv_name):
+    csv_file_path = os.path.join(csv_folder, category_csv_name + ".csv")
     en_tete = ["Title", "Product page URL", "UPC", "Price incl. Tax", "Price excl. Tax", "Number available", "Product Description", "Category", "Review Rating", "Product image url"]
-    with open(csv_filename, "w") as csv_file:
+    with open(csv_file_path, "w") as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
         writer.writerow(en_tete)
         for i in range(len(product_title_list)):
             row = [product_title_list[i], product_page_url_list[i], universal_product_code_list[i], price_including_tax_list[i], price_excluding_tax_list[i], number_available_list[i], product_description_list[i], category_list[i], "'" + review_rating_list[i] + "'", image_url_list[i]]
             writer.writerow(row)
+
+
+def images_download(image_folder):
+    for image_url, title, category in zip(image_url_list, product_title_list, category_list):
+        # Créer un dossier pour la catégorie si elle n'existe pas déjà
+        category_folder = os.path.join(image_folder, category.lower().replace(" ", "_"))
+        os.makedirs(category_folder, exist_ok=True)
+        
+        # Extraire le nom du fichier de l'URL de l'image
+        image_filename = re.sub(r'\W+', '_', title) + ".jpg"
+        image_path = os.path.join(category_folder, image_filename)
+        
+        # Télécharger et enregistrer l'image
+        response = requests.get(image_url)
+        with open(image_path, 'wb') as image_file:
+            image_file.write(response.content)
