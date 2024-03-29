16b62a2d19888226f87ed1ea510972869f35a619
Damien Vincelot <damien.vincelot@gmail.com>
2024-03-29 12:45:17 +0100

    add main script

--- booktoscrape.py
+++ booktoscrape.py
@@ -1,7 +1,5 @@
-# Import des modules, fonctions et listes nécessaires
 import requests
 from bs4 import BeautifulSoup
-from pathlib import Path
 import os
 import time
 from utils import collect_product_urls, collect_product_data, write_data_csv, images_download
@@ -17,26 +15,23 @@ os.makedirs(csv_folder, exist_ok=True)
 image_folder = "images_directory"
 os.makedirs(image_folder, exist_ok=True)
 
+response = requests.get("https://books.toscrape.com/catalogue/page-1.html")
+soup = BeautifulSoup(response.content, "html.parser")
+category_link = soup.find("ul", class_="nav nav-list").find("ul").find_all("a", href=True)
 
-response = requests.get("https://books.toscrape.com/catalogue/page-1.html") # Obtention du contenu de la page
+for category_item in category_link:
+    full_category_link = "https://books.toscrape.com/catalogue/" + category_item['href']
+    category_link_list.append(full_category_link)
+    category_csv_name = full_category_link.split('/')[-2].split('_')[0]
+    collect_product_urls(full_category_link)
 
-soup = BeautifulSoup(response.content, "html.parser") # Parse le contenu de la page html
-category_link = soup.find("ul", class_="nav nav-list").find("ul").find_all("a", href=True) # Recherche de l'url de chaque catégorie
-
-for category_item in category_link: # Boucle qui pour chaque lien de catégorie trouvé dans la page d'accueil :
-    full_category_link = "https://books.toscrape.com/catalogue/" + category_item['href'] # Recompose le lien de catégorie trouvé pour qu'il soit exploitable"
-    category_link_list.append(full_category_link) # Ajoute le lien exploitable à la liste category_link_list
-    category_csv_name = full_category_link.split('/')[-2].split('_')[0] # Sélectionne, dans le lien exploitable, l'intitulé pour une catégorie donnée
-    collect_product_urls(full_category_link) # Appel de la fonction de collecte de l'ensemble des liens de produits pour une catégorie donnée
-
-    for product_item in product_link_list: # Pour chaque lien produit trouvé dans la fonction précédente :
-        collect_product_data(product_item) # Appelle la fonction de collecte de l'ensemble des données pour un produit donné
+    for product_item in product_link_list:
+        collect_product_data(product_item)
 
     images_download(image_folder)
 
-    write_data_csv(csv_folder, category_csv_name) # Appel de la fonction de création et d'écriture de fichier .csv avec les données collectées
+    write_data_csv(csv_folder, category_csv_name)
 
-    # Lorsqu'un fichier csv est créé, réinitialisation des listes pour que chaque fichier csv ne comporte que les données de sa catégorie
     product_link_list.clear()
     product_title_list.clear()
     product_page_url_list.clear()
