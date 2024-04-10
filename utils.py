c97f856d26359aaf5b0156f98899749e9abb9ecd
Damien Vincelot <damien.vincelot@gmail.com>
2024-04-10 16:31:46 +0200

    update

--- utils.py
+++ utils.py
@@ -1,115 +1,143 @@
-# Import des modules nécessaires
 import requests
 from bs4 import BeautifulSoup
-import csv
 import os
+import csv
 import re
 
-# Création des listes nécessaires à la collecte des données
-product_link_list = []
-product_title_list = []
-product_page_url_list = []
-universal_product_code_list = []
-price_including_tax_list = []
-price_excluding_tax_list = []
-number_available_list = []
-product_description_list = []
-category_list = []
-review_rating_list = []
-image_url_list = []
-count = []
 
-# Définition de la fonction de recherche des urls produits et gestion des pages
-def collect_product_urls(full_category_link):
-    category_page = [full_category_link]
-    category_name = full_category_link.split('/')[-2].split('_')[0].capitalize()
-    total_product_link_by_category = 0
-    
-    while category_page:
-        current_page = category_page.pop(0)
-        response = requests.get(current_page)
+session = requests.Session()
+
+
+
+def get_all_page_urls(category_url):
+
+    page_urls_list = []
+    current_url = category_url
+
+    while True:
+        response = session.get(current_url)
+        if response.status_code != 200:
+            print(f"Erreur lors de la récupération de {current_url}")
+            break
+
+        page_urls_list.append(current_url)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        next_page_link = soup.find('li', class_='next')
+        if next_page_link:
+            href_next_page_url = next_page_link.find("a")["href"]
+            next_page_url = category_url.replace("index.html", href_next_page_url)
+        else:
+            break
+
+        current_url = next_page_url
+
+    return page_urls_list
+
+
+
+def get_all_products_urls(page_urls_list):
+
+    product_urls_list = []
+
+    for page_url in page_urls_list:
+        response = session.get(page_url)
+        if response.status_code != 200:
+            print(f"Erreur lors de la récupération de {page_url}")
+            continue
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        product_links_list = soup.find_all("a", title = True)
+        for product_link in product_links_list:
+            href_product_url = product_link["href"]
+            product_url = href_product_url.replace(
+            "../../../","https://books.toscrape.com/catalogue/")
+            product_urls_list.append(product_url)
+
+    return product_urls_list
+
+
+
+def get_all_product_data(product_urls_list):
+
+    all_product_data = []
+
+    for product_url in product_urls_list:
+        response = session.get(product_url)
+        if response.status_code != 200:
+            print(f"Erreur lors de la récupération de {product_url}")
+            continue
+
         soup = BeautifulSoup(response.content, "html.parser")
-        product_link = soup.find_all("a", title=True)
 
-        next_page = soup.find("li", class_="next")
-        if next_page:
-            next_page_link = next_page.find("a")["href"]
-            url_part = current_page.split("/")[-1]
-            next_page_url = current_page.replace(url_part, next_page_link)
-            category_page.append(next_page_url)
+        product_title = soup.find("h1").string
+        product_page_url = product_url
+        universal_product_code = soup.find("td").string
+        product_price_including_tax = soup.find("table", class_="table table-striped").find_all("td")[3].string
+        product_price_excluding_tax = soup.find("table", class_="table table-striped").find_all('td')[2].string
+        product_number_available = soup.find("p", class_="instock availability").text.strip()
+        product_description = soup.find("article", class_="product_page").find_all("p")[3].string
+        product_category_name = soup.find("ul", class_="breadcrumb").find_all("li")[2].text.strip()
+        product_review_rating = str(soup.find("div", class_="col-sm-6 product_main").find_all("p")[2])
+        if "One" in product_review_rating:
+            product_review_rating = "1/5"
+        elif "Two" in product_review_rating:
+            product_review_rating = "2/5"
+        elif "Three" in product_review_rating:
+            product_review_rating = "3/5"
+        elif "Four" in product_review_rating:
+            product_review_rating = "4/5"
+        elif "Five" in product_review_rating:
+            product_review_rating = "5/5"
+        else:
+            product_review_rating = "N/A"
 
-        for product_item in product_link:
-            full_product_link = product_item["href"].replace("../../../", "https://books.toscrape.com/catalogue/")
-            product_link_list.append(full_product_link)
-            count.append(full_product_link)
-            total_product_link_by_category += 1
+        product_image_url = soup.find("div", class_="item active").find("img")["src"].replace("../../", "https://books.toscrape.com/")
 
-    print(f"Nombre de produits trouvés dans la catégorie {category_name}:", total_product_link_by_category)
-    print("Nombre total de produits trouvés : ", len(count))
+        all_product_data.append([product_title, product_page_url, universal_product_code, product_price_including_tax, product_price_excluding_tax, product_number_available, product_description, product_category_name, product_review_rating, product_image_url])
 
-# Définition de la fonction de collecte des données par produit
-def collect_product_data(product_item):
-    response = requests.get(product_item)
-    soup = BeautifulSoup(response.content, "html.parser")
 
-    product_title = soup.find("h1").string
-    product_title_list.append(product_title)
+    return all_product_data
 
-    product_page_url_list.append(product_item)
 
-    universal_product_code = soup.find("td").string
-    universal_product_code_list.append(universal_product_code)
 
-    price_including_tax = soup.find("p", class_="price_color").string
-    price_including_tax_list.append(price_including_tax)
+def write_csv_files(product_urls_list, category_name):
+        
+    folder_name = "csv_directory"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
-    price_excluding_tax = soup.find('table', class_='table').find_all('td')[2].string
-    price_excluding_tax_list.append(price_excluding_tax)
-
-    number_available = soup.find("p", class_="instock availability").text.strip()
-    number_available_list.append(number_available)
-
-    product_description = soup.find("meta", attrs={"name": "description"}).get("content", "").strip()
-    product_description_list.append(product_description)
-
-    category = soup.find("ul", class_="breadcrumb").find_all("li")[2].text.strip()
-    category_list.append(category)
-
-    rating = str(soup.find("div", class_="col-sm-6 product_main").find_all("p")[2])
-    if "One" in rating:
-        review_rating_list.append("1/5")
-    elif "Two" in rating:
-        review_rating_list.append("2/5")
-    elif "Three" in rating:
-        review_rating_list.append("3/5")
-    elif "Four" in rating:
-        review_rating_list.append("4/5")
-    else:
-        review_rating_list.append("5/5")
-
-    image_url = "https://books.toscrape.com/" + soup.find("div", class_="item active").find("img")["src"]
-    image_url_list.append(image_url.strip())
-
-# Définition de la fonction d'écriture des fichiers csv
-def write_data_csv(csv_folder, category_csv_name):
-    csv_file_path = os.path.join(csv_folder, category_csv_name + ".csv")
-    en_tete = ["Title", "Product page URL", "UPC", "Price incl. Tax", "Price excl. Tax", "Number available", "Product Description", "Category", "Review Rating", "Product image url"]
+    csv_file_path = os.path.join(folder_name, category_name + ".csv")
+        
     with open(csv_file_path, "w") as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
-        writer.writerow(en_tete)
-        for i in range(len(product_title_list)):
-            row = [product_title_list[i], product_page_url_list[i], universal_product_code_list[i], price_including_tax_list[i], price_excluding_tax_list[i], number_available_list[i], product_description_list[i], category_list[i], "'" + review_rating_list[i] + "'", image_url_list[i]]
-            writer.writerow(row)
+        writer.writerow(["Title", "Product page URL", "UPC", "Price incl. Tax", "Price excl. Tax", "Number available", "Product Description", "Category", "Review Rating", "Product image url"])
+        
+        for product_url in product_urls_list:
+            product_data = get_all_product_data([product_url])
+            for data in product_data:
+                writer.writerow(data)
 
-# Définition de la fonction de téléchargement des images
-def images_download(image_folder):
-    for image_url, title, category in zip(image_url_list, product_title_list, category_list):
-        category_folder = os.path.join(image_folder, category.lower().replace(" ", "_"))
-        os.makedirs(category_folder, exist_ok=True)
 
-        image_filename = re.sub(r'\W+', '_', title) + ".jpg"
-        image_path = os.path.join(category_folder, image_filename)
 
-        response = requests.get(image_url)
-        with open(image_path, 'wb') as image_file:
+def get_images_jpg(category_name, product_data):
+
+    main_folder = "images_directory"
+    if not os.path.exists(main_folder):
+        os.makedirs(main_folder)
+    category_folder = os.path.join(main_folder, category_name.replace(" ", "_"))
+    if not os.path.exists(category_folder):
+        os.makedirs(category_folder)
+
+    for data in product_data:
+        image_url = data[-1]
+        product_title = data[0]
+        file_name = re.sub(r'\W+', '_', product_title)
+        image_name = file_name + ".jpg"
+        image_path = os.path.join(category_folder, image_name)
+
+        response = session.get(image_url)
+        with open(image_path, "wb") as image_file:
             image_file.write(response.content)
